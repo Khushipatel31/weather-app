27f88bf6c7af0da8f64cbadf4ce81acddfaadd32
Khushipatel31 <khushipatel200431@gmail.com>
2024-06-10 17:37:54 +0530

    showing data in frontend

--- server.js
+++ server.js
@@ -1,30 +1,49 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
 const axios = require("axios");
-app.use(bodyParser.urlencoded({ extended: true }));
 require('dotenv').config();
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set("view engine", "ejs")
-app.use(express.static("public"))
+let weatherData = {
+    city: '',
+    success: null,
+    tempp: '',
+    des: '',
+    iconUrl: '',
+    humidity: '',
+    windSpeed: ''
+};
+
 app.get("/", (req, res) => {
-    const sendData = { location: "Location", temp: "Temp", desc: "Description", feel: "Feel-like", humidity: "Humidity", speed: "Speed" }
-    res.render("index",{sendData})
-})
+    res.render("index", weatherData);
+});
+
 app.post("/", async (req, res) => {
-    let location = await req.body.city;
+    const location = req.body.city;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.APIKEY}&units=metric`;
-    const result = await axios.get(url);
-    const tempp = Math.floor(result.data.main.temp);
-    const des = result.data.weather[0].description;
-    const icon = result.data.weather[0].icon;
-    const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png?`;
-    res.write(`<h1>Current weather in ${location} is ${des} </h1>`);
-    res.write(`<h1>The current temperature is ${tempp}degree celcius</h1>`);
-    res.write(`<img src=${iconUrl}/>`)
-    res.end();
-})
+    try {
+        const result = await axios.get(url);
+        if (result.data.cod === 200) {
+            const tempp = Math.floor(result.data.main.temp);
+            const des = result.data.weather[0].description;
+            const icon = result.data.weather[0].icon;
+            const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+            const city = result.data.name;
+            const humidity = result.data.main.humidity;
+            const windSpeed = result.data.wind.speed;
+            weatherData = { city, success: true, tempp, des, iconUrl, humidity, windSpeed };
+        } else {
+            weatherData = { city: location, success: false, tempp: '', des: '', iconUrl: '', humidity: '', windSpeed: '' };
+        }
+    } catch (error) {
+        weatherData = { city: location, success: false, tempp: '', des: '', iconUrl: '', humidity: '', windSpeed: '' };
+    }
+    res.redirect('/');
+});
 
 app.listen(3000, () => {
-    console.log("server is running on http://localhost:3000")
-})
+    console.log("Server is running on http://localhost:3000");
+});
