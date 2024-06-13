c53f51f63c63dd685f540c4fcd2809a5a6194baf
Khushipatel31 <khushipatel200431@gmail.com>
2024-06-13 20:10:12 +0530

    changed api and fetching data

--- server.js
+++ server.js
@@ -14,32 +14,55 @@ let weatherData = {
     des: '',
     iconUrl: '',
     humidity: '',
-    windSpeed: ''
+    windSpeed: '',
+    forecasts:[],
+    minTemps:[],
+    maxTemps:[]
 };
 
 app.get("/", (req, res) => {
     res.render("index", weatherData);
 });
+const getDayOfWeek = (dateString) => {
+    const date = new Date(dateString);
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return daysOfWeek[date.getUTCDay()];
+};
+
+const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const options = { month: 'short', day: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+};
 
 app.post("/", async (req, res) => {
     const location = req.body.city;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.APIKEY}&units=metric`;
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=${process.env.FORECASTAPIKEY}&q=${req.body.city}&days=7&aqi=no&alerts=no`;
     try {
         const result = await axios.get(url);
-        if (result.data.cod === 200) {
-            const tempp = Math.floor(result.data.main.temp);
-            const des = result.data.weather[0].description;
-            const icon = result.data.weather[0].icon;
-            const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-            const city = result.data.name;
-            const humidity = result.data.main.humidity;
-            const windSpeed = result.data.wind.speed;
-            weatherData = { city, success: true, tempp, des, iconUrl, humidity, windSpeed };
+        console.log(result.data)
+        if (result.status === 200) {
+            const tempp = result.data.current.temp_c;
+            const des = result.data.current.condition.text;
+            const iconUrl = result.data.current.condition.icon;
+            const city = result.data.location.name;
+            const humidity = result.data.current.humidity;
+            const windSpeed = result.data.current.wind_kph;
+            const forecastData=result.data.forecast.forecastday;
+            const forecasts = forecastData.map(data => ({
+                dayOfWeek: getDayOfWeek(data.date),
+                icon: data.day.condition.icon,
+                formattedDate: formatDate(data.date)
+            }));
+            const minTemps = forecastData.map(data => data.day.mintemp_c);
+            const maxTemps = forecastData.map(data => data.day.maxtemp_c);
+            weatherData = { city, success: true, tempp, des, iconUrl, humidity, windSpeed,forecasts,minTemps,maxTemps };
         } else {
-            weatherData = { city: location, success: false, tempp: '', des: '', iconUrl: '', humidity: '', windSpeed: '' };
+            weatherData = { city: location, success: false, tempp: '', des: '', iconUrl: '', humidity: '', windSpeed: '',forecasts:[],minTemps:[],maxTemps:[] };
         }
     } catch (error) {
-        weatherData = { city: location, success: false, tempp: '', des: '', iconUrl: '', humidity: '', windSpeed: '' };
+        console.log(error)
+        weatherData = { city: location, success: false, tempp: '', des: '', iconUrl: '', humidity: '', windSpeed: '' ,forecasts:[],minTemps:[],maxTemps:[]};
     }
     res.redirect('/');
 });
